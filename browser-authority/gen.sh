f07ea70b23880712de44fd0a697272a5f05e769f
thebestornothing <gongzhe@live.com>
2021-05-16 22:25:15 +0800

    add browser-authority

--- browser-authority/gen.sh
+++ browser-authority/gen.sh
@@ -1,39 +1,24 @@
-######################
-# Become a Certificate Authority
-######################
-rm *.key
 rm *.pem
+rm *.key
+rm *.cert
+rm *.req
 rm *.srl
-rm *.ext
-rm *.crt
-rm *.csr
 
-# Generate private key
-openssl genrsa -des3 -out ca.key 2048
-# Generate root certificate
-openssl req -x509 -new -key ca.key -sha256 -days 825 -out ca.cert -subj "/C=FR/ST=Occitanie/O=RootCA/emailAddress=ca@gmail.com"
+# 1. Generate server CA's private key and self-signed certificate
+openssl req -x509 -newkey rsa:4096 -days 365 -nodes -keyout ca.key -out ca.cert -subj "/CN=localhost/emailAddress=ca@gmail.com" 
 
-######################
-# Create CA-signed certs
-######################
+echo "CA's self-signed certificate"
+openssl x509 -in ca.cert -noout -text
 
-NAME=server # Use your own domain name
-# Generate a private key
-openssl genrsa -out $NAME.key 2048
-# Create a certificate-signing request
-openssl req -new -key $NAME.key -out $NAME.csr -subj "/C=FR/ST=Occitanie/O=ServerCA/emailAddress=server@gmail.com"
-# Create a config file for the extensions
->$NAME.ext cat <<-EOF
-authorityKeyIdentifier=keyid,issuer
-basicConstraints=CA:FALSE
-keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
-subjectAltName = @alt_names
-[alt_names]
-DNS.1 = $NAME # Be sure to include the domain name here because Common Name is not so commonly honoured by itself
-DNS.2 = localhost # Optionally, add additional domains (I've added a subdomain here)
-IP.1 = 0.0.0.0  # Optionally, add an IP address (if the connection which you have planned requires it)
-EOF
+# 2. Generate web server's private key and certificate signing request (CSR)
+openssl req -newkey rsa:4096 -nodes -keyout server.key -out server.req -subj "/CN=localhost/emailAddress=server@gmail.com"
 
-# Create the signed certificate
-openssl x509 -req -in $NAME.csr -CA ca.cert -CAkey ca.key -CAcreateserial \
--out $NAME.cert -days 825 -sha256 -extfile $NAME.ext
+# 3. Use CA's private key to sign web server's CSR and get back the signed certificate
+openssl x509 -req -in server.req -days 60 -CA ca.cert -CAkey ca.key -CAcreateserial -out server.cert -extfile server.ext
+
+echo "Server's signed certificate"
+openssl x509 -in server.cert -noout -text
+
+# 4. To verify the server certificate aginst by root CA
+echo "server's certificate verification"
+openssl verify -show_chain -CAfile ca.cert server.cert
