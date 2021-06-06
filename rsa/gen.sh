0b231e6bdaee3fc5dabd6ad6d69d4a0a88c29bed
thebestornothing <gongzhe@live.com>
2021-06-06 18:21:36 +0800

    rsa adapt

--- rsa/gen.sh
+++ rsa/gen.sh
@@ -14,7 +14,7 @@ openssl x509 -in ca.cert -noout -text
 openssl req -newkey rsa:4096 -nodes -keyout server.key -out server.req -subj "/CN=localhost/emailAddress=server@gmail.com"
 
 # 3. Use CA's private key to sign web server's CSR and get back the signed certificate
-openssl x509 -req -in server.req -days 60 -CA ca.cert -CAkey ca.key -CAcreateserial -out server.cert -extfile server-ext.cnf
+openssl x509 -req -in server.req -days 60 -CA ca.cert -CAkey ca.key -CAcreateserial -out server.cert -extfile server.ext
 
 echo "Server's signed certificate"
 openssl x509 -in server.cert -noout -text
@@ -22,3 +22,16 @@ openssl x509 -in server.cert -noout -text
 # 4. To verify the server certificate aginst by root CA
 echo "server's certificate verification"
 openssl verify -show_chain -CAfile ca.cert server.cert
+
+# 5. Generate web client's private key and certificate signing request (CSR)
+openssl req -newkey rsa:4096 -nodes -keyout client.key -out client.req -subj "/CN=localhost/emailAddress=client@gmail.com"
+
+# 3. Use CA's private key to sign web client's CSR and get back the signed certificate
+openssl x509 -req -in client.req -days 60 -CA ca.cert -CAkey ca.key -CAcreateserial -out client.cert -extfile client.ext
+
+echo "client's signed certificate"
+openssl x509 -in client.cert -noout -text
+
+# 4. To verify the client's certificate aginst by root CA
+echo "client's certificate verification"
+openssl verify -show_chain -CAfile ca.cert client.cert
