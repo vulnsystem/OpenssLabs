919a49bdcf3ed740e976bc9ecb9ab5b2494a3aa3
Gitcoins <gitcoins@outlook.com>
2023-02-06 14:44:15 +0800

    Create rsa.js

---
+++ secp256k1/rsa.js
@@ -0,0 +1,40 @@
+const crypto = require('crypto');
+
+crypto.generateKeyPair('rsa', {
+  modulusLength: 4096,
+  publicKeyEncoding: {
+    type: 'spki',
+    format: 'pem',
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8',
+    format: 'pem',
+    cipher: 'aes-256-cbc',
+    passphrase: 'top secret',
+  },
+}, (err, publicKey, privateKey) => {
+  // Handle errors and use the generated key pair.
+  //console.log(publicKey)
+  //console.log(privateKey)
+});
+
+const jose = require('jose')
+
+const generateKey = async () => {
+
+    const algorithm = 'ES256'
+    const x509 = `-----BEGIN CERTIFICATE-----
+    MIIBXjCCAQSgAwIBAgIGAXvykuMKMAoGCCqGSM49BAMCMDYxNDAyBgNVBAMMK3Np
+    QXBNOXpBdk1VaXhXVWVGaGtjZXg1NjJRRzFyQUhXaV96UlFQTVpQaG8wHhcNMjEw
+    OTE3MDcwNTE3WhcNMjIwNzE0MDcwNTE3WjA2MTQwMgYDVQQDDCtzaUFwTTl6QXZN
+    VWl4V1VlRmhrY2V4NTYyUUcxckFIV2lfelJRUE1aUGhvMFkwEwYHKoZIzj0CAQYI
+    KoZIzj0DAQcDQgAE8PbPvCv5D5xBFHEZlBp/q5OEUymq7RIgWIi7tkl9aGSpYE35
+    UH+kBKDnphJO3odpPZ5gvgKs2nwRWcrDnUjYLDAKBggqhkjOPQQDAgNIADBFAiEA
+    1yyMTRe66MhEXID9+uVub7woMkNYd0LhSHwKSPMUUTkCIFQGsfm1ecXOpeGOufAh
+    v+A1QWZMuTWqYt+uh/YSRNDn
+    -----END CERTIFICATE-----`
+    const ecPublicKey = await jose.importX509(x509, algorithm)
+    const obj = new crypto.KeyObject(ecPublicKey);
+    console.log(obj)
+}
+generateKey();
